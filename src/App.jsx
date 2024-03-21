c07581daf3624aff975a1c106fd0a0e0fd909f55
titopope@hotmail.com <titopope@hotmail.com>
2024-03-21 19:08:04 +0100

    antes de clase

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,47 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App () {
-  <></>
+  const [pokeName, setPokeName] = useState('')
+  const [pokeData, setPokeData] = useState([])
+ 
+
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
+      .then((response) => response.json())
+      .then(data => setPokeData([data]));
+  }, [pokeName]);
+  
+  const handleNameChange = (e) => {
+    setPokeName(e.target.value);
+  };  
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    };
+
+  return (
+  <>
+  <div>
+    <h1>Pokemon search</h1>
+    <form onSubmit={handleSubmit} className='search-bar'>
+      <label htmlFor='name'> Nombre </label>
+      <input type="text" name="name" id="name" placeholder="Enter Pokemon Name..." onChange={handleNameChange}/>
+      <button type="submit"> Buscar Pokemon </button>
+    </form>
+  </div>
+  <div className="card">
+    
+      {pokeData.map((pokemon) =>(
+      <div key={pokemon.id}>
+        <h1>{pokemon.name}</h1>
+
+      {/*<img src= {pokemon.sprites.other.dream_world.front_default} alt={pokemon.name}/>*/}
+      </div>))}
+   
+  </div>
+  </>
+  )
 };
 
 export default App;
---
+++ src/Error.jsx
@@ -0,0 +1,11 @@
+
+
+function Error () {
+    return (
+        <>
+        <h3>Esto es un 404</h3>
+        </>
+    )
+}
+
+export default Error
